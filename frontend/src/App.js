17c9b6cc4a705031cde0d1c373886b4852cd6265
Sujal Chordia <sujalchordia10@gmail.com>
2024-01-17 17:42:52 +0530

    signup/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar"
 import './App.css';
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <SignUp isSignedinvar={false}/>
     </div>
   );
 }
