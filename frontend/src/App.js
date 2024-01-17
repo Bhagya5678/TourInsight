588ce36eeed396bbe2c4984a29c296f5aab08fa7
Bhagya Bijlaney <bbijlaney2@gmail.com>
2024-01-17 13:56:51 +0530

    tailwind done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hiiii</h1>
+      <h1 className="bg-blue-500 text-white underline">Hiiii</h1>
     </div>
   );
 }
