2d7d4aa33a2b8496ca271693c23c9e2cae533087
Bhagya Bijlaney <bbijlaney2@gmail.com>
2024-01-17 14:36:31 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h2 className='underline'>H2</h2>
+      <Navbar/>
     </div>
   );
 }
